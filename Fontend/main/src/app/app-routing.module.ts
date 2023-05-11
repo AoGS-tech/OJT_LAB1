9cd5f1237bd1ea4fc6565120d3fe63ca73a25c52
AoGS-tech <110282191+AoGS-tech@users.noreply.github.com>
2023-05-11 13:47:26 +0700

    Up

--- Fontend/main/src/app/app-routing.module.ts
+++ Fontend/main/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'user-management', component: UserManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
